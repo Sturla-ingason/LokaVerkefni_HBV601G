2dea6ef7f9f8b91cba7363951d284267a56e6818
Sturla-ingason <Sturladi@gmail.com>
2026-03-21 01:02:00 +0000

    Byrjað að vinna í userPage
    Byrjaði að setja upp gruning fyrir user page byrjaði að skoða hvernig á að útfæra það og hvernig hún á að lýtta út

--- app/src/main/java/main/app/views/auth/LoginFragment.kt
+++ app/src/main/java/main/app/views/auth/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import main.app.MainActivity
 import main.app.R
 import main.app.databinding.FragmentLoginnBinding
 import main.app.serviceModel.AuthModel
